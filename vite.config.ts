ca773f8ef7e7a2928ff51f8be7aa72962cb15a0d
piyawat_nut <piyawat.su54@gmail.com>
2021-04-14 21:34:24 +0700

    Update: init react with SSR

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  esbuild: {
+    jsxInject: `import React from 'react';`
+  },
+  build: {
+    minify: false
+  }
 })
